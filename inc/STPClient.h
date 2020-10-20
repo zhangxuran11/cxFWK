10bcae686629bd48b989160826d78ec5257420a3
zxr <zxr@example.com>
2020-10-20 21:43:28 +0800

    xxx

---
+++ inc/STPClient.h
@@ -0,0 +1,99 @@
+#ifndef ECHO_CLIENT_H
+#define ECHO_CLIENT_H
+#include <string>
+#include <sstream>
+#include <boost/asio.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+namespace cxFWK{
+    class STPClient{
+        public:
+            STPClient():mSocket(mIoContext){
+            }
+            int connect(const char* serverIp,uint16_t port){
+                boost::system::error_code ec;
+                mSocket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(serverIp),port),ec);
+                if( ec )
+                    return -1;
+                else
+                    return 0;
+            }
+            int connect(const char* eurekaIp,uint16_t eurekaPort,const char* serviceName){
+                mEurekaIp = eurekaIp;
+                mEurekaPort = eurekaPort;
+                mServiceName = serviceName;
+                return connect();
+            }
+            
+            ~STPClient(){
+                mSocket.close();
+            }
+            boost::property_tree::ptree request(const std::string& topic,const boost::property_tree::ptree& pt){
+                std::stringstream ss;
+                boost::property_tree::ptree root;
+                root.put("topic",topic);
+                root.put_child("content",pt);
+                boost::property_tree::json_parser::write_json(ss,root);
+                boost::system::error_code ec;
+                mSendBuffer.resize(ss.str().size()+4);
+                *reinterpret_cast<uint32_t*>(mSendBuffer.data()) = ss.str().size();
+                memcpy(mSendBuffer.data()+4,ss.str().data(),ss.str().size());
+                boost::asio::write(mSocket,boost::asio::buffer(mSendBuffer,mSendBuffer.size()),ec);
+                if(ec)
+                {
+                    return boost::property_tree::ptree();
+                }
+                mRecvBuffer.resize(4);
+                boost::asio::read(mSocket,boost::asio::buffer(mRecvBuffer,mRecvBuffer.size()),ec);
+                if(ec)
+                {
+                    return boost::property_tree::ptree();
+                }
+                uint32_t len = *reinterpret_cast<uint32_t*>(mRecvBuffer.data());
+                mRecvBuffer.resize(len);
+                boost::asio::read(mSocket,boost::asio::buffer(mRecvBuffer,mRecvBuffer.size()),ec);
+                if(ec)
+                {
+                    return boost::property_tree::ptree();
+                }
+                ss.str("");
+                ss<<std::string(mRecvBuffer.data(),mRecvBuffer.size());
+                boost::property_tree::ptree res;
+                boost::property_tree::json_parser::read_json(ss,res);
+                return res.get_child("content");
+            }
+        private:
+            int connect(){
+                boost::system::error_code ec;
+                mSocket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(mEurekaIp),mEurekaPort),ec);
+                if( ec )
+                    return -1;
+                boost::property_tree::ptree pt;
+                pt.put("service name",mServiceName);
+                pt = request("query service",pt);
+                if(pt.count("service address") < 1){
+                    return -1;
+                }
+                std::string serviceAddr = pt.get<std::string>("service address");
+                char ip[20];
+                uint16_t port;
+                sscanf(serviceAddr.c_str(),"%s:%hu",ip,&port);
+                mSocket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string(ip),port),ec);
+                if( ec )
+                    return -1;
+                
+                return 0;
+            }
+        private:
+            std::string mEurekaIp;
+            uint16_t mEurekaPort;
+            std::string mServiceName;
+            boost::asio::io_context mIoContext;
+            boost::asio::ip::tcp::socket mSocket;
+            std::vector<char> mRecvBuffer;
+            std::vector<char> mSendBuffer;
+
+    };
+}
+
+#endif//ECHO_CLIENT_H
---
+++ inc/WebSocketServer.h
@@ -0,0 +1,106 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio.hpp>
+namespace cxFWK{
+    class WebSocketServer{
+        boost::asio::io_context mIoContext;
+        boost::asio::ip::tcp::acceptor mAcceptor;
+
+        class Session
+        : public std::enable_shared_from_this<Session>{
+            
+            public:
+                Session(WebSocketServer &server,boost::asio::ip::tcp::socket& socket):mServer(server),mWs(std::move(socket)){
+                    //std::cout<<"Session()"<<std::endl;
+                }
+
+                ~Session(){
+                    //std::cout<<"~Session()"<<std::endl;
+                }
+                void run(){
+                    doRead();
+                }
+            private:
+                void doRead(){
+                    auto self = shared_from_this();
+                    mRecvBuffer.resize(4);
+                    boost::asio::async_read(mSocket,boost::asio::buffer(mRecvBuffer, mRecvBuffer.size()),
+                    [this, self](boost::system::error_code ec, std::size_t length)
+                    {
+                        if (!ec && length > 0)
+                        {
+                            std::uint32_t len = * reinterpret_cast<std::uint32_t*>(mRecvBuffer.data());
+                            mRecvBuffer.resize(len);
+                            auto self = shared_from_this();
+                            boost::asio::async_read(mSocket,boost::asio::buffer(mRecvBuffer, mRecvBuffer.size()),
+                            [this, self](boost::system::error_code ec, std::size_t length)
+                            {
+                                if (!ec && length > 0){
+                                    boost::property_tree::ptree pt;
+                                    std::stringstream sstream(std::string(mRecvBuffer.data(),mRecvBuffer.size()));
+                                    boost::property_tree::json_parser::read_json(sstream, pt);
+                                    std::string cmd;
+                                    try{
+                                        cmd = pt.get<std::string>("topic");
+                                    }
+                                    catch (boost::exception& e){
+                                        std::cout<<boost::diagnostic_information(e)<<std::endl;
+                                    }
+                                    boost::property_tree::ptree res;
+                                    if(mServer.mProcesserSet.count(cmd) > 0){
+                                        res.put_child("content",std::move(mServer.mProcesserSet[cmd](pt.get_child("content"))));
+                                    }
+                                    else{
+                                        res.put_child("content",std::move(boost::property_tree::ptree()));
+                                    }
+                                    
+                                    sstream.str("");
+                                    std::stringstream ss;
+                                    boost::property_tree::json_parser::write_json(sstream,res);
+                                    mSendBuffer.resize(sstream.str().size()+4);
+                                    *reinterpret_cast<std::uint32_t*>(mSendBuffer.data()) = sstream.str().size();
+                                    memcpy(mSendBuffer.data()+4,sstream.str().data(),sstream.str().size());
+                                    boost::asio::async_write(mSocket,boost::asio::buffer(mSendBuffer.data(),mSendBuffer.size()),
+                                        [this,self](boost::system::error_code ec, std::size_t length){
+                                            if(!ec){
+                                                doRead();
+                                            }
+                                            else{ //应答失败
+                                            }
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            private:
+                WebSocketServer& mServer;
+                boost::beast::websocket mWs;
+                std::vector<char> mRecvBuffer;
+                std::vector<char> mSendBuffer;
+        };
+
+        public:
+            WebSocketServer(uint16_t port):mAcceptor(mIoContext, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port)){
+                mAcceptor.set_option(boost::asio::socket_base::reuse_address(true));
+            }
+        void run(){
+            doAccept();
+            mIoContext.run();
+        }
+        void doAccept(){
+            mAcceptor.async_accept(
+            [this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket)
+            {
+            if (!ec)
+            {
+                std::make_shared<Session>(*this,socket)->run();
+            }
+            doAccept();
+            });
+
+        }
+
+    };
+
+}
